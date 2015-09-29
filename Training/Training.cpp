84b3a4e04687bcc02ae15faec6a3f9f40eb7d95b
Gen Onodera <genpiichaan@gmail.com>
2015-09-29 15:20:51 +0900

    stack

--- Training/Training.cpp
+++ Training/Training.cpp
@@ -33,7 +33,7 @@ void stack::push(char ch)
 	tos++;
 }
 
-void stack::pop()
+char stack::pop()
 {
 	if (tos == 0){
 		cout << "スタックは空です";
@@ -47,8 +47,20 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	char ch;
 	stack s1, s2;
+	int i;
 
-	
+	s1.init();
+	s2.init();
+	s1.push('a');
+	s2.push('x');
+	s1.push('b');
+	s2.push('y');
+	s1.push('c');
+	s2.push('z');
+	for (i = 0; i < 3; i++) cout << "pop s1: " << s1.pop() << "\n";
+	for (i = 0; i < 3; i++) cout << "pop s2: " << s2.pop() << "\n";
+
+	// q で終了
 	do {
 		cin >> ch;
 	} while (ch != 'q');
