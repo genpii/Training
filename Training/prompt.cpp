06d5ed252fab29efb6432a50a841f40f0697ff69
Gen Onodera <genpiichaan@gmail.com>
2015-09-30 17:57:31 +0900

    prompt

---
+++ Training/prompt.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class prompt {
+	int count;
+public:
+	prompt(char *in);
+	~prompt();
+};
+
+prompt::prompt(char *in)
+{
+	cout << in;
+	cin >> count;
+}
+
+prompt::~prompt()
+{
+	int i, j;
+	for (i = 0; i < count; i++){
+		Beep(13000, 4000);
+		//for (j = 0; j < 100000; j++);
+	}
+}
+
+int main()
+{
+	prompt p("input number: ");
+
+	return 0;
+}
